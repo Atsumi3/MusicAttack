4d5e6b1c25f0643d14d75a073409e1fe5f1a7b55
Atsumi <atsumi3041@yahoo.co.jp>
2012-06-21 01:17:19 +0900

    Judgment Update

---
+++ class.js
@@ -0,0 +1,83 @@
+//クラスセット
+var Pon = enchant.Class.create(enchant.Sprite,{
+	initialize: function(x,y,img,img_num,img_size,button,obj){
+		enchant.Sprite.call(this,img_size,img_size);
+		this.x = x; this.y = y;
+		this.image = img; this.time = 0;
+		this.duration = 20; this.frame = 0;
+		this.addEventListener('enterframe',function(){
+			this.time++;
+			this.frame = Math.floor(this.time/this.duration * img_num);
+			if(this.time == this.duration) this.remove(obj);
+		});
+		this.addEventListener(Event.TOUCH_START,function(e){
+			for(var i=0;i<button.length;i++)
+			{
+				var b = button[i];
+				if(b.x < e.x && b.y < e.y && b.x + b.width > e.x && b.y + b.height > e.y)
+				{
+					b.dispatchEvent(e);
+				}
+			}
+		});
+		obj.addChild(this);
+	},
+	remove:  function(obj){obj.removeChild(this);}
+});
+var Button = enchant.Class.create(enchant.Sprite,{
+	initialize: function(x,y,img,img_size,obj){
+		enchant.Sprite.call(this,img_size,img_size);
+		this.x = x; this.y = y; this.image = img;
+		obj.addChild(this);
+	},
+	remove: function(obj){obj.removeChild(this);}
+});
+
+var ShowLabel = Class.create(Label,{
+	initialize:function(obj,x,y,text){
+		enchant.Label.call(this);
+		this.x = x; this.y = y; this.text = text;
+		obj.addChild(this);
+	}
+});
+var HitLabel = Class.create(Label,{
+	initialize:function(obj,text,color,x,y){
+		enchant.Label.call(this);
+		this.text = text;
+		this.font = "16px monospace";
+		this.color = color;
+		this.x = x; this.y = y;
+		obj.addChild(this);
+		this.tick = 0;
+		this.addEventListener(Event.ENTER_FRAME, function(){
+			this.y -= 0.5;
+			this.tick ++;
+			if(this.tick>40) obj.removeChild(this);
+		});
+	}
+});
+var Bar = Class.create(Sprite,{
+	initialize:function(sx,sy,x,y){
+		enchant.Sprite.call(this,sx,sy);
+		this.x = x; this.y = y;
+		var barElement1 = new Surface(280,10);
+		var gradObj = barElement1.context.createLinearGradient( 0, 0, 0, 8);
+		gradObj.addColorStop(   1, '#7F3F3F');
+		gradObj.addColorStop(   0.3, '#3F7F3F');
+		barElement1.context.fillStyle = gradObj;
+		barElement1.context.fillRect(0,0,this.width,this.height);
+		this.image = barElement1;
+		this.image.context.beginPath();
+		this.image.context.fillRect(20,10,300,20);
+	}
+});
+
+var Score = Class.create({
+	initialize:function(){
+		this.score = 0;
+		this.Toscore = 0;
+	},
+	add:function(_score){
+		this.Toscore += _score;
+	}
+});
---
+++ system.js
@@ -0,0 +1,73 @@
+enchant();
+
+//***定数セット***//
+var MSize = 100; //ボタンの大きさ
+var BACK_IMG = "img/back.jpg";
+var Title = music[0];
+var Music = music[1];
+var M_time = music[2];
+var Composer = music[3];
+var Diff = music[4];
+var SET_ANIM = "img/hanabi.png";
+var SET_ANIM_x = 200; //アニメーション画像の横幅
+var SET_ANIM_NUM = 12; //枚数
+var SET_MARK = "img/mark.gif";
+var SET_PROG = "img/seek.png";
+var FULL_COMBO = "song/fullcombo.mp3"; //フルコンボの音
+var PlayFlag = true;
+
+//***グローバル変数***//
+var combo = 0;
+var Maxcombo = 0;
+var EndMark = 0; //終わった譜面カウント
+var seekW = 280 /M_time; //進行度
+var NowPlay = 0;//連続再生防止用
+
+var button = []; //領域配列
+var mark = []; //マーク配列
+
+//***グループ***//
+var ShowData = Group();  //スコア等
+var GameField = Group(); //♂領域
+var lane = new Group();    //マーク
+//***グローバル関数***//
+function format(fmt) {
+  for (i = 1; i < arguments.length; i++) {
+    var reg = new RegExp("\\{" + (i - 1) + "\\}", "g")
+    fmt = fmt.replace(reg,arguments[i]);
+  }
+  return fmt;
+}
+function calc(base,val){
+	return res = base + val*96;
+}
+function calc_note(ary){
+	var count = 0;
+	var i = 100;
+	while(ary[i] != 'end')
+	{
+		count++; i++;
+		if(count> 3000) break;
+	}
+	return count;
+}
+
+function readMark(ary,count)
+{
+	result = ary[100+count].split(":");
+	return [result[0],result[1]];
+}
+
+Surface.prototype.drawCircle = function(x,y,r){
+	this.context.beginPath();
+	this.context.arc(x,y,r,0,Math.PI*2,true);
+	this.context.stroke();
+}
+
+var elem = calc_note(music);
+var setMark = [];
+for(var i=0;i<elem;i++)
+{
+	setMark[i] = readMark(music,i);
+}
+var exScore = Math.floor(50000/elem);
