0e28651c805a7be2ce597399df425da18b94b1ae
Atsumi <atsumi3041@yahoo.co.jp>
2012-12-12 01:48:49 +0900

    Load Fix

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 *
 *The MIT License
 *
-*Copyright (c) 2012 @atm_tk
+*Copyright (c) 2012 atm_tk
 *
 *Permission is hereby granted, free of charge, to any person obtaining a copy
 *of this software and associated documentation files (the "Software"), to deal
